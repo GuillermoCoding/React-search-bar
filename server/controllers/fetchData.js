c004157df2f8f7e5615f732281fefa89e2959c05
Guillermo Saavedra <gs22525@email.vccs.edu>
2018-03-27 14:11:53 -0400

    heroku

--- server/controllers/fetchData.js
+++ server/controllers/fetchData.js
@@ -4,6 +4,8 @@ require('dotenv').config();
 module.exports = async(input)=>{
     const response = await axios(`https://maps.googleapis.com/maps/api/place/autocomplete/json?input=${input}&key=${process.env.GOOGLE_API_KEY}`);
     const results = response.data.predictions;
+    console.log('here');
+    console.log(results);
     const array =  results.map(result=>{
       return result.description;
     });
