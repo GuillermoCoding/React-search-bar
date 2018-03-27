cf43ec8a4b0401bee3a7a5fcd64f1757fcf4c919
Guillermo Saavedra <gs22525@email.vccs.edu>
2018-03-27 12:35:21 -0400

    Apollo client setup

---
+++ client/SearchBar.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class SearchBar extends Component {
+    render(){
+        return (
+            <input/>
+        );
+    }
+
+}
+
+export default SearchBar;
--- client/index.js
+++ client/index.js
@@ -1,11 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import SearchBar from './SearchBar';
+import { ApolloClient } from 'apollo-client';
+import { HttpLink } from 'apollo-link-http';
+import { InMemoryCache } from 'apollo-cache-inmemory';
+import { ApolloProvider } from 'react-apollo';
 
+const cache = new InMemoryCache();
+
+const client = new ApolloClient({
+    link: new HttpLink({uri: 'http://localhost:4000/graphql'}),
+    cache
+});
 const Root = ()=>{
     return (
+        <ApolloProvider client={client}>
         <div>
-            SearchBar
+            <SearchBar/>
         </div>
+        </ApolloProvider>
     );
 };
 
