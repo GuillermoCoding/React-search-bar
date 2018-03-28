babd3571a7a9c5fc51dfa50e886ad754c65cd5d7
Guillermo Saavedra <gs22525@email.vccs.edu>
2018-03-27 20:03:20 -0400

    indentation

--- client/SearchBar.js
+++ client/SearchBar.js
@@ -6,57 +6,57 @@ import styles from './styles/SearchBar.css';
 
 class SearchBar extends Component {
   constructor(props){
-	  super(props);
-	  this.state = {
-		  input: '',
-		  items: []
-		}
-	}
-	onChange(selectedItem){
-		this.setState({input: selectedItem});
-	}
-	async onInputValueChange(input){
-		await this.setState({input});
-		const results = await this.props.client.query({
-			query: fetchSuggestions,
-			variables: {
-				input: this.state.input
-			}
-		});
-		this.setState({items: results.data.suggestions});
-	}
-	render(){
-		return (
-			<DownShift
-				inputValue={this.state.input}
-				onChange={this.onChange.bind(this)}
-				onInputValueChange={this.onInputValueChange.bind(this)}
-				render={({getInputProps,getItemProps,isOpen, selectedItem,highlightedIndex})=>(
-					<div>
-						<input placeholder={'Start typing..'} className={styles.searchBar} {...getInputProps()} type="text"/>
-						{isOpen?(
-							<div className={styles.results}>
-								{this.state.items.map((result,index)=>{
-									return (
-										<div
-											className={styles.item} 
-											{...getItemProps({item: result})} 
-											key={result}
-											style={{
-												backgroundColor: highlightedIndex === index ? 'rgb(179, 218, 255)' : 'white'
-											}}
-										>
-											{result}
-										</div>
-									);
-								})}
-							</div>
-						):null}
-					</div>
-				)}
-			/>
-		);
-	}
+    super(props);
+    this.state = {
+      input: '',
+      items: []
+}
+}
+onChange(selectedItem){
+this.setState({input: selectedItem});
+}
+async onInputValueChange(input){
+await this.setState({input});
+const results = await this.props.client.query({
+query: fetchSuggestions,
+variables: {
+input: this.state.input
+}
+});
+this.setState({items: results.data.suggestions});
+}
+render(){
+return (
+<DownShift
+inputValue={this.state.input}
+onChange={this.onChange.bind(this)}
+onInputValueChange={this.onInputValueChange.bind(this)}
+render={({getInputProps,getItemProps,isOpen, selectedItem,highlightedIndex})=>(
+<div>
+<input placeholder={'Start typing..'} className={styles.searchBar} {...getInputProps()} type="text"/>
+{isOpen?(
+<div className={styles.results}>
+{this.state.items.map((result,index)=>{
+return (
+<div
+className={styles.item} 
+{...getItemProps({item: result})} 
+key={result}
+style={{
+backgroundColor: highlightedIndex === index ? 'rgb(179, 218, 255)' : 'white'
+}}
+>
+{result}
+</div>
+);
+})}
+</div>
+):null}
+</div>
+)}
+/>
+);
+}
 }
 
 export default withApollo(SearchBar);
